2eeef9ae9091358df1d11e2d97853f5b96f68841
Natsumi Yuki <natsumiyuuki.a@gmail.com>
2025-11-18 11:27:16 -0300

    att

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from . import views  # Import from current app
+from . import views
 
 urlpatterns = [
-    path('', views.home_view, name='home'),
+    path("", views.home, name="home"),
+    path("criar-lista/", views.criar_lista, name="criar_lista"),
+    path("minhas-listas/", views.minhas_listas, name="minhas_listas"),
+    path("lista/<int:id_lista>/", views.ver_lista, name="ver_lista"),
+    path("lista/<int:id_lista>/add-item/", views.adicionar_item, name="adicionar_item"),
 ]
